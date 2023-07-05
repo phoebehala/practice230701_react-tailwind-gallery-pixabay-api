c89779400df14c60bcfc867e6131da85f6e7ba91
Phoebe <phyuhala@gmail.com>
2023-07-05 12:00:48 -0700

    Show gray circle if there is no user photo

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -5,7 +5,11 @@ const PhotoCard = ({photo}) => {
   return (
     <div>
           <div className= 'flex items-center gap-1 box-border py-8'>
-          <img className='rounded-full w-2/5' src={photo.userImageURL}/> 
+            {
+             photo.userImageURL? <img className='rounded-full w-2/5 w-28 h-28' src={photo.userImageURL}/> 
+             :<div className='rounded-full w-2/5 bg-slate-300 w-28 h-28'></div> 
+            }
+
             <p className='text-xl w-3/5'>{photo.user}</p>
           </div>
 
